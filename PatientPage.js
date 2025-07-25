698d236fa98eaa3a5ad59c7752a25060e49bc807
Abbdelkhalek Mohamed <130919335+2151144@users.noreply.github.com>
2025-07-25 20:47:42 +0300

    Update PatientPage.js

--- PatientPage.js
+++ PatientPage.js
@@ -1,3 +1,12 @@
+/*
+  ==============================================================
+  Author: Abdelkhalek Soudy
+  Project: Dental Disease Detection and Chatbot Assistant (Frontend)
+  Date: 2025
+  All rights reserved © Abdelkhalek Soudy
+  ==============================================================
+*/
+
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "./PatientPage.css"; // استيراد ملف الـ CSS
